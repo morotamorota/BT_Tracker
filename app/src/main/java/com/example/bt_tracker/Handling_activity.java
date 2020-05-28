734939f93a811ff6b9784045663ed94f475ff379
morotamorota <2020m086@kuas.ac.jp>
2020-05-28 23:34:44 +0900

    styled up Button 2.3.4

--- app/src/main/java/com/example/bt_tracker/Handling_activity.java
+++ app/src/main/java/com/example/bt_tracker/Handling_activity.java
@@ -2,7 +2,10 @@ package com.example.bt_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class Handling_activity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class Handling_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_handling_activity);
+    }
+
+    public void goToGod_help(View view) {
+        Intent Ohmygod = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.healthline.com/health/how-to-break-a-fever#TOC_TITLE_HDR_1"));
+        startActivity(Ohmygod);
     }
 }
--- app/src/main/java/com/example/bt_tracker/Mechanism_activity.java
+++ app/src/main/java/com/example/bt_tracker/Mechanism_activity.java
@@ -2,7 +2,10 @@ package com.example.bt_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class Mechanism_activity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class Mechanism_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mechanism_activity);
+    }
+
+    public void goTofall_in_LOVE(View view) {
+        Intent TrueLOVE = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.shutterstock.com/ja/search/its+ok"));
+        startActivity(TrueLOVE);
     }
 }
