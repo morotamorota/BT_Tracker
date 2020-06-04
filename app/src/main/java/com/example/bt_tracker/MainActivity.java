b9b110795770a30522d5340cf980ca18db8e80c5
morotamorota <2020m086@kuas.ac.jp>
2020-06-04 23:55:08 +0900

    implemented OnClickListener.

--- app/src/main/java/com/example/bt_tracker/MainActivity.java
+++ app/src/main/java/com/example/bt_tracker/MainActivity.java
@@ -5,35 +5,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mainButton1 = findViewById(R.id.Button_1st);
+        mainButton1.setOnClickListener(this);
+
+        TextView mainButton2 = findViewById(R.id.Button_2nd);
+        mainButton2.setOnClickListener(this);
+
+        TextView mainButton3 = findViewById(R.id.Button_3rd);
+        mainButton3.setOnClickListener(this);
+
+        TextView mainButton4 = findViewById(R.id.Button_4th);
+        mainButton4.setOnClickListener(this);
     }
 
-    public void goToLog_activity(View view) {
-        Intent toLog = new Intent(this, Log_activity.class);
-        startActivity(toLog);
+    @Override
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.Button_1st:
+                Intent toLog = new Intent(this, Log_activity.class);
+                startActivity(toLog);
+               // You can push!!!
+                break;
+
+            case R.id.Button_2nd:
+                Intent toNorm = new Intent(this, Normal_activity.class);
+                startActivity(toNorm);
+                // It`s OK bro
+                break;
+
+            case R.id.Button_3rd:
+                Intent toMecha = new Intent(this, Mechanism_activity.class);
+                startActivity(toMecha);
+                // It`s OK sis
+                break;
+
+            case R.id.Button_4th:
+                Intent toHand = new Intent(this, Handling_activity.class);
+                startActivity(toHand);
+                //Yeah bro
+                break;
+        }
+
     }
 
-
-    public void goToNormal_activity(View view) {
-        Intent toNorm = new Intent(this, Normal_activity.class);
-        startActivity(toNorm);
-    }
-
-    public void goToMechanism_activity(View view) {
-        Intent toMecha = new Intent(this, Mechanism_activity.class);
-        startActivity(toMecha);
-    }
-
-    public void goToHandling_activity(View view) {
-        Intent toHand = new Intent(this, Handling_activity.class);
-        startActivity(toHand);
-    }
 }
 
 
